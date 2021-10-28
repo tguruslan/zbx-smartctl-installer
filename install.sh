97bb2b75594e72f6f667b373c65442eaa01f0040
tguruslan <tguruslan1994@gmail.com>
2021-10-28 05:15:06 +0300

    dynamic get zbx user and group, add inc zabbix_agentd

--- install.sh
+++ install.sh
@@ -20,19 +20,32 @@ fi
 echo "(optional) Install sg3-utils if you need to monitor hardware RAIDs."
 echo "(optional) Install nvme-cli if you need to monitor NVMe devices."
 
+zbx_user=$(cat /etc/passwd | grep zabbix | tr ':' ' ' | awk '{print $1}')
+zbx_group=$(cat /etc/group | grep zabbix | tr ':' ' ' | awk '{print $1}')
+
 mkdir -p tmp
 git clone https://github.com/v-zhuravlev/zbx-smartctl tmp
 
 mkdir -p /etc/sudoers.d/
+sed -i "s/Defaults:zabbix/Defaults:${zbx_user}/g" tmp/sudoers_zabbix_smartctl
+sed -i "s/zabbix ALL/${zbx_user} ALL/g" tmp/sudoers_zabbix_smartctl
 cp tmp/sudoers_zabbix_smartctl /etc/sudoers.d/
 chmod 440 /etc/sudoers.d/sudoers_zabbix_smartctl
 
 mkdir -p /etc/zabbix/zabbix_agentd.d
 cp tmp/zabbix_smartctl.conf /etc/zabbix/zabbix_agentd.d/
+chown -R $zbx_user:$zbx_group /etc/zabbix/zabbix_agentd.d
+
+if egrep -i "^Include=" /etc/zabbix/zabbix_agentd.conf; then
+    sed -i "s/^Include=.*/Include=/etc/zabbix/zabbix_agentd.d/*.conf/g" /etc/zabbix/zabbix_agentd.conf
+  else
+    echo -e "# Include zabbix_agentd.d" >> /etc/zabbix/zabbix_agentd.conf
+    echo -e "Include=/etc/zabbix/zabbix_agentd.d/*.conf" >> /etc/zabbix/zabbix_agentd.conf
+fi
 
 mkdir -p /etc/zabbix/scripts
 cp tmp/discovery-scripts/nix/smartctl-disks-discovery.pl /etc/zabbix/scripts/
-chown zabbix:zabbix /etc/zabbix/scripts/smartctl-disks-discovery.pl
+chown -R $zbx_user:$zbx_group /etc/zabbix/scripts
 chmod u+x /etc/zabbix/scripts/smartctl-disks-discovery.pl
 
 
